1b3ad378a2794193668e5e90804ced57d7963f31
Naldson Chagas <naldson.bc@gmail.com>
2020-05-19 15:20:27 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      host: '',
+      synchronize: true,
+      username: '',
+      password: '',
+      autoLoadEntities: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
