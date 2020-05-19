414cd948504326db483536e4cca9f225bfe76ee9
Naldson Chagas <naldson.bc@gmail.com>
2020-05-19 17:42:14 -0300

    easy config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,20 @@ import { PurchaseModule } from './purchase/purchase.module';
 import { CategoryModule } from './category/category.module';
 import { UserModule } from './user/user.module';
 import { InstallmentsModule } from './installments/installments.module';
+import { EasyconfigModule } from 'nestjs-easyconfig';
 
 @Module({
   imports: [
+    EasyconfigModule.register({
+      path: './.env'
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
       synchronize: true,
-      username: 'postgres',
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true
     }),
     PurchaseModule,
