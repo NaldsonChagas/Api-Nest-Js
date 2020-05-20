3003f51c9c39ae73bb70cfca8c79fb00e07a1309
Naldson Chagas <naldson.bc@gmail.com>
2020-05-20 20:27:50 -0300

    listando compras por usuário

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body, ValidationPipe, Req } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, Req, Delete, Get, Param } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 import { InstallmentsService } from 'src/installments/installments.service';
 import { Request } from 'express';
 import { UserService } from 'src/user/user.service';
 import { CategoryService } from 'src/category/category.service';
 import { Purchase } from './purchase.entity';
+import { User } from 'src/user/user.entity';
 
 @Controller('purchase')
 export class PurchaseController {
@@ -18,12 +19,19 @@ export class PurchaseController {
   async create (@Body(new ValidationPipe()) purchase, @Req() request: Request) {
     purchase.user = await this.userService
       .findOne(Number(request.headers.userid));
-
     purchase.category = await this.categoryService
       .findOne(purchase.category_id);
 
     const savedPurchase: Purchase = await this.purchaseService.save(purchase);
-    return await this.installmentsService
+    await this.installmentsService
       .save(savedPurchase, new Date(purchase.date + 'T00:00'), purchase.installments);
+    return savedPurchase;
+  }
+
+  @Get()
+  async find (@Req() request: Request) {
+    const userId = Number(request.headers.userid);
+    const user: User = await this.userService.findOne(userId);
+    return await this.purchaseService.findByUser(user);
   }
 }
--- src/purchase/purchase.service.ts
+++ src/purchase/purchase.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Repository, DeleteResult } from 'typeorm';
 import { Purchase } from './purchase.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
 
 @Injectable()
 export class PurchaseService {
   constructor (@InjectRepository(Purchase)
-    private puchaseRepository: Repository<Purchase>) {}
+    private purchaseRepository: Repository<Purchase>) {}
+
+  async findByUser (user: User): Promise<Purchase[]> {
+    return this.purchaseRepository.find({
+      where: {
+        user
+      },
+      relations: ['category']
+    });
+  }
 
   async save (purchase: Purchase): Promise<Purchase> {
-    return this.puchaseRepository.save(purchase);
+    return this.purchaseRepository.save(purchase);
+  }
+
+  async delete (purchaseId: number): Promise<DeleteResult> {
+    return this.purchaseRepository.delete(purchaseId);
   }
 }
